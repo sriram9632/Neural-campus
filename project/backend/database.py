86e47695fa0313c77561845f14ac883ab60e1294
sriram9632 <gnirup2006@gmail.com>
2026-06-22 11:28:32 +0530

    Change MongoDB password in database.py
    Updated MongoDB connection string with a new password.

--- project/backend/database.py
+++ project/backend/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb+srv://nirup:nirup@cluster0.fm1ehpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://nirup:password@cluster0.fm1ehpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = AsyncIOMotorClient(MONGO_URL)
 
